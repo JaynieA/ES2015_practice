539bc2cb32342d7f48186f8ad5f444db1c311bc2
Jaynie Anderson <mrs.jaynie.anderson@gmail.com>
2017-02-28 15:23:50 -0600

    define differences between let and var

--- app.js
+++ app.js
@@ -1,3 +1,48 @@
 console.log('app.js sourced');
 
-//Declarations with let
+// ** -- Variable Declarations -- ** //
+
+// -- Declarations with var -- //
+var petsArray = ['Reggie', 'Willie', 'Hector'];
+var loadPetNames = function(petsArray) {
+  if (petsArray.length > 2) {
+    var loadingMessage = 'Wait for it...';
+    console.log('1.', flashMessage); //undefined (out of scope)
+  } else {
+    var flashMessage = "Loading";
+    console.log('2.', flashMessage);
+  } // end else
+  console.log('3.', flashMessage); //undefined (out of scope)
+}; // end loadPetNames
+loadPetNames(petsArray);
+
+// -- Declarations with let -- //
+var printChoreName = function(first, second) {
+  console.log(first);
+}; // end printChoreName
+
+var choresArray = ['Dust', 'Vacuum', 'Sweep'];
+var loadChoreNames = function(choresArray) {
+  if (choresArray.length > 2) {
+    let loadingMessage = 'Wait for it...';
+    console.log('1.', flashMessage); //ReferenceError
+  } else {
+    let flashMessage = "Loading";
+    console.log('2.', flashMessage);
+  } // end else
+  console.log('3.', flashMessage); //ReferenceError
+}; // end loadChoreNames
+//loadChoreNames(choresArray);
+
+/* Variables declared with `let` can be redefined,
+but cannot be redeclared within the same scope */
+let myName = 'Jaynie';
+myName = 'Jayne'; // redefinition is allowed
+
+let currentDay = "Monday";
+//let currentDay = 'Tuesday'; //this will cause a TypeError
+
+
+
+
+//Declarations with const
--- readme.md
+++ readme.md
@@ -5,3 +5,12 @@ Client-side JavaScript, focused on practical implementation of the most general 
 * variable and function declarations
 * Data Structures
 * Promises and Generators
+
+# Variable Declarations
+
+## let variables
+
+* Benefits:
+  * Using `let` instead of `var` prevents variable declarations from being moved to the top of the scope on what is known as hoisting.
+  * With `let`, there's no sharing in `for` loops. A new variable is created on each iteration.
+  * Variables declared with `let` can be redefined, but cannot be redeclared within the same scope.
